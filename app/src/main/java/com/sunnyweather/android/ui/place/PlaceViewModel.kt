9e929e84cc43899385998b5f2924af3ca758b24a
haha <971757231@qq.com>
2020-11-24 13:07:25 +0800

    v3

---
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -0,0 +1,21 @@
+package com.sunnyweather.android.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.sunnyweather.android.logic.Repository
+import com.sunnyweather.android.logic.model.Place
+
+class PlaceViewModel : ViewModel() {
+    private val searchLiveDate = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveDate) { query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query : String){
+        searchLiveDate.value = query
+    }
+}
